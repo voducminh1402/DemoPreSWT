a40289c1ad7ecddd41d307251243b9f858f4a644
voducminh1402 <voducminh140201@gmail.com>
2022-02-16 09:05:01 +0700

    add cover

--- src/demo/manager/DemoTestFunction.java
+++ src/demo/manager/DemoTestFunction.java
@@ -2,13 +2,14 @@ package demo.manager;
 
 public class DemoTestFunction {
 	public static int playWithFive(int num) {
+		int flag = 0;
 		if (num > 5) {
-			return 5 * num;
+			flag = 5 * num;
 		}
-		return num;
+		return flag;
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(playWithFive(8));
+		playWithFive(8);
 	}
 }
